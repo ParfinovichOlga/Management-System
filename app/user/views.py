d378ad34eae00705c9e20a50031acca5a99a2869
ParfinovichOlga <parfinovich.olga@yandex.ru>
2025-06-11 12:24:57 +0300

    Added project

---
+++ app/user/views.py
@@ -0,0 +1,22 @@
+"""
+Views for the user API.
+"""
+from rest_framework import generics, permissions
+from user.serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
